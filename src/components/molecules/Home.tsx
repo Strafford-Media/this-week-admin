91586e6b3313536d36650ffff58f293d070166cf
tehpsalmist <bstewardcodes@gmail.com>
2024-02-10 22:47:17 -0500

    home

--- src/components/molecules/Home.tsx
+++ src/components/molecules/Home.tsx
@@ -4,8 +4,13 @@ export interface HomeProps extends ComponentProps<'div'> {}
 
 export const Home = ({ className = '', ...props }: HomeProps) => {
   return (
-    <div className={`${className}`} {...props}>
-      This Week Hawaii Website Admin Dashboard
+    <div className={`${className} flex-center h-content flex-col p-4`} {...props}>
+      <img
+        className="max-w-2/3"
+        src="https://lirp.cdn-website.com/0e650340/dms3rep/multi/opt/twhawaii-logo-1920w.png"
+        alt="This Week Hawaii"
+      />
+      <h2 className="text-2xl text-primary-500 lg:text-4xl">Website Management</h2>
     </div>
   )
 }
